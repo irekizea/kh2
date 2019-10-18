ee0d0a7fe9c83ca01b341cfcaca8e26ec0a6670e
irekizea <56664373+irekizea@users.noreply.github.com>
2019-10-18 11:24:52 +0900

    dsf
    sdfsdf

---
+++ 2019_10_18/src/variable/test01.java
@@ -0,0 +1,20 @@
+package variable;
+
+import java.lang.*;
+
+public class test01 {
+	public static void main(String[] args) {
+		//정수 : 소수점이 없는 숫자
+		//[1] 계산중에 범위를 넘어가면 뒤집힘 실수는 순환형이 아님
+		//[2] 자신의 형태에 맞는 데이터만 대입 가능
+		int a = 10;
+		int b = 100 * 200;
+		byte c = (byte)500;
+		int e = 10 % 3;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(e);
+	}
+
+}
+                                                                                      
