a412edf4bb3617ba1e84a56cc82f55db5be3f0bb
irekizea <56664373+irekizea@users.noreply.github.com>
2019-10-25 17:57:35 +0900

    1밀리초간 대기

---
+++ Day07/src/loop/Test16.java
@@ -0,0 +1,28 @@
+package loop;
+import java.util.Scanner;
+public class Test16 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("분 입력");
+		int min = sc.nextInt();
+		System.out.println("초 입력");
+		int s = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(min +"분" + s+"초 시작");
+		
+		int sA = min *60 + s;
+		
+		for(int i= sA; i>0; i--) {
+			min = i/60;
+			s = i - min * 60;
+			System.out.println(min + "분" + s + "초 남음");
+			Thread.sleep(1000L); // 1000밀리초간 대기
+		}
+		
+		System.out.println("TimeOver");
+	}
+
+}
