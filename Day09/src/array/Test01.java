e9bc57fd8104b4ef513dac4a0f9996d98e5bb36d
irekizea <56664373+irekizea@users.noreply.github.com>
2019-10-29 09:59:14 +0900

    array

---
+++ Day09/src/array/Test01.java
@@ -0,0 +1,18 @@
+package array;
+import java.util.Scanner;
+public class Test01 {
+	public static void main(String[] args) {
+		int a, b, c, d, e;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자 5 개를 입력 하세요");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		e = sc.nextInt();
+		sc.close();
+		System.out.println(a +"  " + b+ "  " + c + "   " +d+ "  " +e);
+		
+	}
+
+}
---
+++ Day09/src/array/Test02.java
@@ -0,0 +1,22 @@
+package array;
+import java.util.Scanner;
+public class Test02 {
+	public static void main(String[] args) {
+		int[]/*인트 묶음*/ t = new int[5];
+		// t(리모컨)------->[0][0][0][0][0]
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자 5 개를 입력 하세요");
+		t[0] = sc.nextInt();
+		t[1] = sc.nextInt();
+		t[2] = sc.nextInt();
+		t[3] = sc.nextInt();
+		t[4] = sc.nextInt();
+		sc.close();
+		System.out.println(t[0] +"  " + t[1]+ "  " + t[2] + "   " +t[3]+ "  " +t[4]);
+		
+	}
+
+}
