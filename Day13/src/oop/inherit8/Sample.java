a48c7638b6527c63b401ed80df3787f254863a4c
irekizea <56664373+irekizea@users.noreply.github.com>
2019-11-04 16:14:07 +0900

    color chooser

--- Day13/src/oop/inherit8/Sample.java
+++ Day13/src/oop/inherit8/Sample.java
@@ -1,8 +1,10 @@
 package oop.inherit8;
 
+import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
-public class Sample extends JFileChooser{//JFrame
-
+//public class Sample extends JFileChooser{
+//public class Sample extends JFrame
+	public class Sample extends JColorChooser{
 }
--- Day13/src/oop/inherit8/Test01.java
+++ Day13/src/oop/inherit8/Test01.java
@@ -4,7 +4,8 @@ public class Test01 {
 	public static void main(String[] args) {
 		Sample s = new Sample();
 //		s.setVisible(true);
-		s.showOpenDialog(null);
+//		s.showOpenDialog(null);
+		s.showDialog(null, null, null);
 	}
 	
 }
