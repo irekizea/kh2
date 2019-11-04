c7cb33f6d25b7be74a38bd3e3cfb4dda8b7da96b
irekizea <56664373+irekizea@users.noreply.github.com>
2019-11-04 16:10:25 +0900

    file chooser

--- Day13/src/oop/inherit8/Sample.java
+++ Day13/src/oop/inherit8/Sample.java
@@ -1,7 +1,8 @@
 package oop.inherit8;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
-public class Sample extends JFrame{//
+public class Sample extends JFileChooser{//JFrame
 
 }
--- Day13/src/oop/inherit8/Test01.java
+++ Day13/src/oop/inherit8/Test01.java
@@ -3,7 +3,8 @@ package oop.inherit8;
 public class Test01 {
 	public static void main(String[] args) {
 		Sample s = new Sample();
-		s.setVisible(true);
+//		s.setVisible(true);
+		s.showOpenDialog(null);
 	}
 	
 }
