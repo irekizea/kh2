12b6af541b23ff4961f76197ef1832b06a5e3379
irekizea <56664373+irekizea@users.noreply.github.com>
2019-11-06 13:42:39 +0900

    제곱

---
+++ Day15/src/api/lang/string/Test07.java
@@ -0,0 +1,16 @@
+package api.lang.string;
+
+public class Test07 {
+	public static void main(String[] args) {
+//		Math class
+//		Math a = new Math();
+		
+		System.out.println(Math.PI);
+		
+		int a = 5;  //radius
+		
+		System.out.println(a*a*Math.PI);
+		System.out.println(Math.pow(5,  2)*Math.PI);
+	}
+
+}
