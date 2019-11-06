84a84ed402047680861ac01ee6b063933e257549
irekizea <56664373+irekizea@users.noreply.github.com>
2019-11-06 12:17:20 +0900

    인트 변환

--- Day15/src/api/lang/string/Test06.java
+++ Day15/src/api/lang/string/Test06.java
@@ -2,7 +2,20 @@ package api.lang.string;
 
 public class Test06 {
 	public static void main(String[] args) {
+//		원시형 자료형과 문자열과의 상호 변환
+		int  a = 100;
 		
+//		String b = a;
+//		String b = (String)b;
+		String b = ""+a;
+		System.out.println(b);
+		
+		String c = String.valueOf(a);
+		
+		String d = "345";
+//		int e = d;
+		int e = Integer.parseInt(d);
+		System.out.println(e);
 	}
 
 }
